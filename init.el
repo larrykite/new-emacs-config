f30489b5bba5c861138fdc0855c8b1c15a64bd85
Larry Kite <larrykite@gmail.com>
2013-01-24 15:25:44 -0800

    added solarized and zenburn themes

--- init.el
+++ init.el
@@ -84,3 +84,15 @@
 ;; menu bar is useful when getting started
 (menu-bar-mode)
 (setq-default default-tab-width 4)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("27470eddcaeb3507eca2760710cc7c43f1b53854372592a3afa008268bcf7a75" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
